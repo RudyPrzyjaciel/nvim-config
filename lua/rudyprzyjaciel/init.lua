e3ccdb7a2dcac975db33066751293dc8b58b5f35
RudyPrzyjaciel <szynks20@gmail.com>
2024-01-11 19:38:07 +0100

    RPs nvim config

---
+++ lua/rudyprzyjaciel/init.lua
@@ -0,0 +1,38 @@
+require("rudyprzyjaciel.set")
+require("rudyprzyjaciel.remap")
+
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " " 
+
+
+require("lazy").setup({
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }},
+    { "mbbill/undotree" },
+
+    --- Uncomment the two plugins below if you want to manage the language servers from neovim
+    --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+})
